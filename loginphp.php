34d429ecdcf4ae5c9dbe377988fc9cb2a662cd41
Dhaval Jariwala <dhaval.jariwala98@gmail.com>
2020-09-23 19:08:13 +0530

    Update loginphp.php

--- loginphp.php
+++ loginphp.php
@@ -8,6 +8,7 @@
 session_start();
 //$link = mysqli_connect("localhost", "root", "", "wd");	
 $link = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "bfa2a557a3ad14", "69ba10e2", "heroku_a5da83056c5ca5c");	
+//$link = mysqli_connect("us-cdbr-east-02.cleardb.com", "b207cf51f7f376", "95a19b5b", "heroku_ec8d6bfe8530fc1");	
 if (isset($_POST['email']) )
 {
 
