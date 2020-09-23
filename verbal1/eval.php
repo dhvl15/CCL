c5914ef3caa6974d070119cccbcb2b6d2110b497
Dhaval Jariwala <dhaval.jariwala98@gmail.com>
2020-09-23 19:17:50 +0530

    Update eval.php

--- verbal1/eval.php
+++ verbal1/eval.php
@@ -1,7 +1,8 @@
 <?php 
 
 //$conn = mysqli_connect("127.0.0.1", "root", "", "wd");
-$conn = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "bfa2a557a3ad14", "69ba10e2", "heroku_a5da83056c5ca5c");
+//$conn = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "bfa2a557a3ad14", "69ba10e2", "heroku_a5da83056c5ca5c");
+$conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "b207cf51f7f376", "95a19b5b", "heroku_ec8d6bfe8530fc1");
 session_start();
 $verbal_finalcount = 0;
 $q1 = array();
